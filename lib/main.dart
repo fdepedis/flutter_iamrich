e2da47dc3789bc6afda6b9ee4efd9936a2506910
Flavio <flavio.depedis@gmail.com>
2020-09-23 22:37:25 +0200

    insert background, appBar, body and Image

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("I Am Rich"),
+          title: Center(child: Text("I Am Rich")),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+          ),
         ),
       ),
     ),
